804cbbfd9ea429ed52bc44b55e10aa63da55ddb8
charlesjbasedow <cbasedow@outlook.com>
2022-02-07 04:44:36 -0500

    reset button added

--- js/app.js
+++ js/app.js
@@ -19,15 +19,17 @@ let gameWinner = null
 
 /*------------------------ Cached Element References ------------------------*/
 
-let boardSqs = document.querySelectorAll('.square')
-let statusMsg = document.querySelector('.game-status')
-let board = document.querySelector('.board')
+const boardSqs = document.querySelectorAll('.square')
+const statusMsg = document.querySelector('.game-status')
+const board = document.querySelector('.board')
+const resetBtn = document.querySelector('.reset-btn')
 
 
 
 
 /*----------------------------- Event Listeners -----------------------------*/
 board.addEventListener('click', handleClick)
+resetBtn.addEventListener('click', init)
 
 
 /*-------------------------------- Functions --------------------------------*/
@@ -35,7 +37,7 @@ board.addEventListener('click', handleClick)
 init()
 
 function init () {
-  boardArray = [null, null, null, null, null, null, null, null]
+  boardArray = [null, null, null, null, null, null, null, null, null]
   playerTurn = 1
   gameWinner = null
   
@@ -43,7 +45,8 @@ function init () {
 }
 
 function render() {
-boardArray.forEach((sqr, idx) => {
+  
+  boardArray.forEach((sqr, idx) => {
   let squareLetter
   if (boardArray[idx] === 1) {
     squareLetter = 'X'
@@ -52,13 +55,9 @@ boardArray.forEach((sqr, idx) => {
   } else if (boardArray[idx] === null) {
     squareLetter = ''
   }
-  
-  
-  currentGameStatus()
+  boardSqs[idx].textContent = squareLetter
 })
-
-function currentGameStatus() {
-  if (gameWinner !== null) {
+  if (gameWinner === null) {
     statusMsg.textContent = `Player ${playerTurn === 1 ? 'X' : 'O'}, it's your turn to act`
   } else if (gameWinner === 'T') {
     statusMsg.textContent = 'This match resulted in a DRAW!'
@@ -66,7 +65,7 @@ function currentGameStatus() {
     statusMsg.textContent = `Congrats ${gameWinner === 1 ? 'X' : 'O'}'s! You are the WINNER`
 }
 }
-}
+
 
 function handleClick(evt) {
   let squareIdx = parseInt(evt.target.id.replace('sq', ''))
@@ -98,28 +97,3 @@ function handleClick(evt) {
     }
   }
   
-  
-	  
-	   
-		  // 5.6.1.1) Loop through the each of the winning combination arrays defined.
-		  // 5.6.1.2) Total up the three board positions using the three indexes in the current combo.
-		  // 5.6.1.3) Convert the total to an absolute value (convert any negative total to positive).
-		  // 5.6.1.4) If the total equals 3, we have a winner! Set the winner variable to the board's value at the index specified by the first index of that winning combination's array by returning that value.
-
-
-		// 5.6.3) Next, If there's no winner, check if there's a tie:
-
-		// 5.6.4) Set the winner varible to "T" if there are no more nulls in the board array by returning the string "T".
-	  
-		// 5.6.5) Otherwise return null.
-
-// 5.7) All state has been updated, so render the state to the page (step 3.3).
-
-
-// 6) Handle a player clicking the replay button:
-
-	// 6.1) Add a replay button to the HTML document
-
-	// 6.2) Store the new replay button element
-
-	// 6.3) Do steps 4.1 (initialize the state variables) and 4.2 (render).
